350208941c536af8de73a42559d1692382bb354a
JamesIsNinja <jamesisninja@hotmail.com>
2016-03-09 01:55:17 -0500

    Added Import statements for all libraries needed for calculations

--- app/src/main/java/com/example/music/androidstudiocalculator/MainActivity.java
+++ app/src/main/java/com/example/music/androidstudiocalculator/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.music.androidstudiocalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
