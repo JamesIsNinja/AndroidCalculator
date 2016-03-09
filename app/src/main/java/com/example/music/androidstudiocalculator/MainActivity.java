7b2be3fa93f86cf2520f4ed8bb1390c6b7d8c881
JamesIsNinja <jamesisninja@hotmail.com>
2016-03-09 01:58:26 -0500

    added IDs for all the buttons, textview and booleans for errors, the decimal

--- app/src/main/java/com/example/music/androidstudiocalculator/MainActivity.java
+++ app/src/main/java/com/example/music/androidstudiocalculator/MainActivity.java
@@ -9,6 +9,19 @@ import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
+    // IDs of all the numbers
+    private int[] numericButtons = {R.id.buttonZero, R.id.buttonOne, R.id.buttonTwo, R.id.buttonThree, R.id.buttonFour, R.id.buttonFive,
+                                    R.id.buttonSix, R.id.buttonSeven, R.id.buttonEight, R.id.buttonNine};
+    // IDs of all the operators
+    private int[] operatorButtons = {R.id.buttonAdd, R.id.buttonSubtract, R.id.buttonMultiply, R.id.buttonDivide};
+    // TextView used to display the output
+    private TextView calcArea;
+    // checks whether the lastly pressed key is numeric or not
+    private boolean lastNumeric;
+    // checks that current state is in error or not
+    private boolean stateError;
+    // If true, do not allow to add another DOT
+    private boolean lastDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
